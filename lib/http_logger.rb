d9497b2247fd7d6d21d4bcd04714080bd54bb612
Darryl Almonte <almonte23@gmail.com>
2022-02-17 15:47:51 -0400

    fixes for truncated STATSD URL

--- lib/http_logger.rb
+++ lib/http_logger.rb
@@ -238,10 +238,10 @@ class HttpLogger
 	end
 
 	def truncate_url_for_statsd(http, request, response = nil)
-		hostname = "#{http.address}:#{http.port}"
-		http_response_code = response.nil? ? "" : response.code
+		hostname = "#{http.address}"
+		http_response_code = response.nil? ? "" : "#{request.method}.#{response.code}"
 		path_split = request.path.nil? ? "" : request.path.split('/').reject {|c| c.blank?}.take(self.class.statsd_max_url_element)
-		self.class.statsd_prefix + hostname + path_split.join('') + request.method + http_response_code
+		hostname + path_split.join('.') + http_response_code
 	end
 
 end
